0a86f02de4a8f3489e838674b92dfc958a063c0d
lovelysehotra <lsehotra12@gmail.com>
2024-10-11 16:04:09 +0530

    change socket config

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 import express from "express";
 import mongoose from "mongoose";
 import 'dotenv/config'
+import http from "http";
 // import cors from 'cors';
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/AuthRoutes.js";
 import { userDeserializer } from "./middleware/userDeserializer.js";
-
-
-import { app,server } from "./Socket/Socket.js";
 import userRoutes from "./routes/UserRoutes.js";
+import socketConnection from "./Socket/Socket.js";
+import { Server } from "socket.io";
 let PORT = process.env.PORT || 3000;
+const app = express()
+const server = http.createServer(app);
+export const io = new Server(server,{
+    maxHttpBufferSize:1e8,
+    cors:{
+        origin:'*',
+        methods:["GET","POST"]
+    }
+})
+const socket = new socketConnection(io);
+socket.connect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(userDeserializer);
@@ -26,5 +37,5 @@ mongoose
     .connect(process.env.DB_LOCATION, {
         autoIndex: true
     })
-    .then(() => console.log("DB Connecti on Successfully"))
+    .then(() => console.log("DB Connection Successfully"))
     .catch((error) => console.log(error));
